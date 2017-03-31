b07b6f7a9682ab426eac8204636a04b784f722f6
trevor <trevor@10.0.0.183>
2017-03-30 20:34:04 -0400

    Added a started BankSimulation Class.

---
+++ CSC202BankSim/csc202banksim/BankSimulation.java
@@ -0,0 +1,75 @@
+package csc202banksim;
+
+import java.util.Scanner;
+
+public class BankSimulation {
+
+	
+	private int simulationLength;
+	private int arrivalLow;
+	private int arrivalHigh;
+	private int serviceLow;
+	private int serviceHigh;
+	private int numCustomers;
+	private int totalWaitTime;
+	private int nextArrival;
+	private int nextOpening;
+	private int nextDeparture; 
+	private int numTellers;
+	private Bank b;
+	
+	public BankSimulation(){
+		
+	}
+	
+	public BankSimulation(int sLength, int aLow, int aHigh, int sLow, int sHigh, int nTellers){
+		this.simulationLength = sLength;
+		this.arrivalLow = aLow;
+		this.arrivalHigh = aHigh;
+		this.serviceLow = sLow;
+		this.serviceHigh = sHigh;
+		this.numTellers = nTellers;
+		
+	}
+	
+	public void startSimulation(){
+		
+		this.b = new Bank(numTellers);
+		
+		
+		
+		
+	}
+	
+	public void inputParameters(){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("What is the length of the simultaion in minutes?");
+		simulationLength = scan.nextInt();
+		
+		System.out.println("How many available tellers?");
+		numTellers = scan.nextInt();
+		
+		System.out.println("What is the shortest time between customer arrivals?");
+		arrivalLow = scan.nextInt();
+		
+		System.out.println("What is the longest time between customer arrivals?");
+		arrivalHigh = scan.nextInt();
+		
+		System.out.println("What is the shortest time needed for customer service?");
+		serviceLow = scan.nextInt();
+		
+		System.out.println("What is the longest time needed for customer service?");
+		serviceHigh = scan.nextInt();
+	}
+	
+	public void displayResults(){
+		
+	}
+	
+	public int randomTime(int low, int high){
+		int diff = high - low + 1;
+		int num = ((int) (Math.random()*diff ))+ low;
+		return num;
+	}
+}
---
+++ CSC202BankSim/csc202banksim/Test1.java
@@ -0,0 +1,11 @@
+package csc202banksim;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		BankSimulation b = new BankSimulation();
+		System.out.println(b.randomTime(1, 6));
+
+	}
+
+}
